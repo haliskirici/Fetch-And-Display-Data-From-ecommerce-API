62e2787ad0c1bc5eb2931e23298f571be29a2bba
haliskirici <81765983+haliskirici@users.noreply.github.com>
2021-05-15 16:53:53 +0300

    Update App.js
    Installing react native and building the foundation of first page.

--- App.js
+++ App.js
@@ -1,16 +1,55 @@
-import React from 'react'
+import React, {Component} from 'react';  
+import {Platform, StyleSheet, Text, View} from 'react-native';  
+  
+type Props = {};  
+export default class App extends Component<Props> {  
+  render() {  
+    return (  
+      <View style={styles.container}>
+        <View style={styles.box1}>
+          <Text style={styles.textStyle}>PRODUCTS</Text>
+        </View>
+        <View style={styles.box2}>
+          <Text style={styles.textStyle}>CATEGORIES</Text>
+        </View>
+        <View style={styles.box3}>
+          <Text style={styles.textStyle}>ORDERS</Text>
+        </View>
+      </View>
+    );  
+  }  
+}  
 
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Initializing
-        </p>
-        <p> There will be options here </p>
-      </header>
-    </div>
-  );
-}
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 100,
+    flex: 1, 
+    flexDirection: 'row',   
+  },
+  box1: {
+    width: 75,
+    height: 75,
+    
+    
+    backgroundColor: 'steelblue'
+  },
+  box2: {
+    width: 75,
+    height: 75,
+    
+    
+    backgroundColor: 'red'
+  },
+  box3: {
+    width: 75,
+    height: 75, 
+  
+    
+    backgroundColor: 'orange'
+  },
+  textStyle: {
+    color: 'black',
+    alignSelf: 'center',
+    margin: 25,
+  }
+});
